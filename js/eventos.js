b4275b13fc70842ae9360574d150bab5b94c9d2e
kitozatoichi <richardcardozo7@gmail.com>
2023-06-08 13:40:43 -0300

    fasdfasdfasd

--- js/eventos.js
+++ js/eventos.js
@@ -1,29 +1,32 @@
-document.getElementById('ordenarpor').addEventListener('change', (e) =>
-        {
-            let datosResultado= new Object();
-            let valorBuscado=document.getElementById('buscador').value.trim();
 
-            if (valorBuscado!=''){
-                datosResultado=filtrarPorTexto(data,valorBuscado);
-                datosResultado=ordenarPorPrecio(datosResultado,e.target.value);
-            }else{
-                datosResultado=ordenarPorPrecio(data,e.target.value);
-            }
-            recargarListaArticulos(datosResultado);
-        });
+document.addEventListener("change", (e) => {
+   if (e.target.id=="buscador"){
+    let valorBuscado= e.target.value;
+    let datos= new Object();
+    datos.productos = filtrarPorTexto(data,valorBuscado);
+    let valorOrden=document.getElementById('ordenarpor').value;
+    if (valorOrden!=''){ datos.productos=ordenarPorPrecio(datos,valorOrden); }
+    recargarListaArticulos(datos);
+   }
+   else if(e.target.id=="ordenarpor"){
+    let datosResultado= new Object();
+    let valorBuscado=document.getElementById('buscador').value.trim();
 
-document.getElementById('buscador').addEventListener('change', (e) =>
-        {
-           let valorBuscado= e.target.value;
-           let datos= new Object();
-           datos.productos =data.productos.filter(item => item.nombre.toUpperCase().includes(valorBuscado.toUpperCase()))
-           let valorOrden=document.getElementById('ordenarpor').value;
-           if (valorOrden!=''){
-            datos=ordenarPorPrecio(datos,valorOrden);
-           }
-           recargarListaArticulos(datos);
+    if (valorBuscado!=''){
+        datosResultado.productos=filtrarPorTexto(data,valorBuscado);
+        datosResultado=ordenarPorPrecio(datosResultado,e.target.value);
+    }else{
+        datosResultado=ordenarPorPrecio(data,e.target.value);
+    }
+    recargarListaArticulos(datosResultado);
+   }   
+});
 
-        });
-
-
+document.addEventListener("click", (e) => {    
+    const { target } = e;
+    if (!target.matches("nav a")) {
+        return;
+    }
+     route(e);
+});
 
